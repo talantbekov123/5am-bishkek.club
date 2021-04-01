73478f2be10f6a3d1e0b60c22006a3c0addf9c47
Kairat Talantbekov <talantbekov123@gmail.com>
2021-04-01 18:20:33 +0600

    Add change

---
+++ Evdon__express-mesto_1617034424/express-mesto-main/controllers/users.js
@@ -0,0 +1,90 @@
+const User = require('../models/user');
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+const getUser = (req, res) => {
+  const id = req.user._id;
+  User.findById(id)
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'пользователь не найден' });
+      }
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некоректные данные' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      }
+      res.status(500).send({ message: 'Произошла ошибка' });
+    });
+};
+
+const updateProfile = (req, res) => {
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name: req.body.name, about: req.body.about }, {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'пользователь не найден' });
+      }
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError' || (err.name === 'CastError')) {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      }
+      res.status(500).send({ message: 'Произошла ошибка' });
+    });
+};
+
+const updateAvatar = (req, res) => {
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar: req.body.avatar }, {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'пользователь не найден' });
+      }
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError' || (err.name === 'CastError')) {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      }
+      res.status(500).send({ message: 'Произошла ошибка' });
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+  updateProfile,
+  updateAvatar,
+};
---
+++ Evdon__express-mesto_1617034424/express-mesto-main/routes/cards.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {
+  getCards,
+  deleteCard,
+  createCard,
+  likeCard,
+  dislikeCard,
+} = require('../controllers/cards');
+
+router.get('/cards', getCards);
+router.get('/cards/:cardId', deleteCard);
+router.post('/cards', createCard);
+router.put('/cards/:cardId/likes', likeCard);
+router.delete('/cards/:cardId/likes', dislikeCard);
+
+module.exports = router;
